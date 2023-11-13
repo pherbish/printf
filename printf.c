e0b7ac6e68b0229ed6dbe5f4ea675de10aca56cb
Tom Morara <tom@geek.com>
2023-11-13 19:03:34 +0300

    function header files main.h and stdarg.h in the function

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  *int _printf(const char *format, ...) - The function tha produces output according to a format.
+  *@const char *format: argument to be printed
+  *@... : The variable number of arguments that can be taken by the function and then printed.
+  * Return: returns an integer
+  */
+int _printf(const char *format, ...)
+{
+
+}
