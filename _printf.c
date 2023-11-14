b3c09c93c47a2e9b244638ddfac6d67e368a66cb
Tom Morara <tom@geek.com>
2023-11-14 23:53:09 +0300

    revised to be betty compliant

--- _printf.c
+++ _printf.c
@@ -1,65 +1,48 @@
 #include "main.h"
-
 /**
-  *_printf- printf function
-  *@format: print format
-  * Return: printed characters
-  */
+ *_printf- printf function
+ *@format: print format
+ * Return: printed characters
+ */
 int _printf(const char *format, ...)
 {
 	int lchar = 0;
 	va_list strargs;
 
 	if (format == NULL)
-	{
-		return (-1);
-	}
+	{return (-1); }
 	va_start(strargs, format);
-
 	while (*format)
-	{
-		if (*format != '%')
-		{
-		write(1, format, 1);
-		lchar++;
-		}
+{
+	if (*format != '%')
+		{write(1, format, 1);
+		lchar++; }
 		else
-		{
-		format++;
-
+		{format++;
 		if (*format == '\0')
 			break;
-
 		if (*format == '%')
-		{
-			write(1, format, 1);
-			lchar++;
-		}
+		{write(1, format, 1);
+			lchar++; }
 		else if (*format == 'd')
-		{
-			int d = va_arg(strargs, int);
-
+		{int d = va_arg(strargs, int);
 		else if (*format == 'c')
-		{
-			char c = va_arg(strargs, int);
+		{char c = va_arg(strargs, int);
 				write(1, &c, 1);
-			lchar++;
-		}
+			lchar++; }
 		else if (*format == 's')
-		{
-			char *s = va_arg(strargs, char *);
+		{char *s = va_arg(strargs, char *);
+
 			int str_len = 0;
 
 			while (s[str_len] != '\0')
-			{
-				str_len++;
+			{str_len++;
+
 				write(1, s, str_len);
-				lchar += str_len;
-			}
+				lchar += str_len; }
 		}
-	}
-	format++;
-	}
+		}
+	format++; }
 va_end(strargs);
 return (lchar);
 }
