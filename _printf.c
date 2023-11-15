debaadf2c7c70ba0e156aa7a3ab9984e1d387ca7
Tom Morara <tom@geek.com>
2023-11-15 04:45:01 +0300

    0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -7,59 +7,40 @@
   */
 int _printf(const char *format, ...)
 {
-	int lchar = 0;
-	va_list strargs;
+	va_list ptr;
+	unsigned int length = 0, Q = 0;
 
 	if (format == NULL)
-	{
 		return (-1);
-	}
-	va_start(strargs, format);
+	va_start(ptr, format);
 
-	while (*format)
+	while (format[Q] != '\0')
 	{
-		if (*format != '%')
+		for (; format[Q] != '%' && format[Q]; Q++)
 		{
-		write(1, format, 1);
-		lchar++;
+			write(1, &format[Q], 1);
+			length++;
 		}
-		else
+		if (!format[Q])
 		{
-		format++;
-
-		if (*format == '\0')
-			break;
-
-		if (*format == '%')
-		{
-			write(1, format, 1);
-			lchar++;
+			return (length);
 		}
-		else if (*format == 'd')
+		if (get_handle_format(&format[Q + 1], &length, ptr))
 		{
-			int d = va_arg(strargs, int);
-
-		else if (*format == 'c')
-		{
-			char c = va_arg(strargs, int);
-				write(1, &c, 1);
-			lchar++;
+			Q += 2;
+			continue;
 		}
-		else if (*format == 's')
+		if (format[Q + 1] == '%')
 		{
-			char *s = va_arg(strargs, char *);
-			int str_len = 0;
-
-			while (s[str_len] != '\0')
-			{
-				str_len++;
-				write(1, s, str_len);
-				lchar += str_len;
-			}
+			write(1, &format[Q], 1);
+			length++;
+			Q += 2;
+			continue;
 		}
+		write(1, &format[Q], 1);
+		length++;
+		Q++;
 	}
-	format++;
-	}
-va_end(strargs);
-return (lchar);
+	va_end(ptr);
+	return (length);
 }
