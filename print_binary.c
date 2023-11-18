edd5d56d79ae56a013510629e81822bdeb237b02
tommorara <119826192+tommorara@users.noreply.github.com>
2023-11-18 03:17:19 +0300

    Add files via upload
    2. With a face like mine, I do better in print

---
+++ print_binary.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * print_bin - prints binary number.
+ * @val: parameter.
+ * Return: integer
+ */
+int print_bin(va_list val)
+{
+	int flag = 0;
+	int cont = 0;
+	int i, a = 1, b;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int p;
+
+	for (i = 0; i < 32; i++)
+	{
+		p = ((a << (31 - i)) & num);
+		if (p >> (31 - i))
+			flag = 1;
+		if (flag)
+		{
+			b = p >> (31 - i);
+			_putchar(b + 48);
+			cont++;
+		}
+	}
+	if (cont == 0)
+	{
+		cont++;
+		_putchar('0');
+	}
+	return (cont);
+}
+
+file: print_c.c
+
+#include "main.h"
+
+/**
+ * print_c - prints a character.
+ * @val: arguments.
+ * Return: integer.
+ */
+int print_c(va_list val)
+{
+	char s;
+
+	s = va_arg(val, int);
+	_putchar(s);
+	return (1);
+}
+
